5fdeca2e6b72344db4a91649b95570bc368d4cbf
wikipop <66514885+wikipop@users.noreply.github.com>
2023-04-23 09:00:21 +0200

    grid with addtional div

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import "./style.scss"
+import React from "react";
+
+function App() {
+
+    const buttonAction = (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault()
+    }
+
+    return (
+    <header>
+        <nav>
+            <ul>
+                <li> <a href="/">Home</a> </li>
+                <li> <a href="/about">About</a> </li>
+                <li> <a href="/contact">Contact</a> </li>
+            </ul>
+        </nav>
+        <main>
+            <div>
+                <div className="title">
+                    <div>
+                        <h1> Hello World </h1>
+                        <h2> Wiktor Popiołek </h2>
+                        <h3> Web developer </h3>
+                    </div>
+                    <div>
+                        <button onClick={buttonAction}>Action 1</button>
+                        <button onClick={buttonAction}>Action 2</button>
+                    </div>
+                </div>
+                <div className="photo">
+                    <img src="https://cdn.discordapp.com/avatars/393756120952602625/2caac8389fb62044770e1ac81fdbbc60.webp?size=320"
+                         alt="profile picture"
+                    />
+                </div>
+            </div>
+        </main>
+    </header>
+  )
+}
+
+export default App
